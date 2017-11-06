17c570ba23344b4ce9b545a18e71d26b92cd0783
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-06 15:26:13 -0200

    Add metodo salvar.

---
+++ src/main/java/br/DaoGeneric.java
@@ -0,0 +1,21 @@
+package br;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.jsf.jpautil.JPAUtil;
+
+public class DaoGeneric<E> {
+	
+	public void salvar(E entidade){
+		EntityManager entityManager = JPAUtil.geEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		entityManager.persist(entidade);
+		
+		entityTransaction.commit();
+		entityManager.close();
+	}
+
+}
