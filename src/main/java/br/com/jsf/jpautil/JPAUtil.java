4fea742e77b11dd27d55a822bad61ad8629244c3
Pedro Santos <pedro_henriquems321@hotmail.com>
2017-11-06 15:16:26 -0200

    Add JPAUtil

---
+++ src/main/java/br/com/jsf/jpautil/JPAUtil.java
@@ -0,0 +1,20 @@
+package br.com.jsf.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static EntityManagerFactory factory = null;
+	static {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory("projeto_JSF");
+		}
+	}
+	
+	public static EntityManager geEntityManager(){
+		return factory.createEntityManager();
+		
+	}
+}
